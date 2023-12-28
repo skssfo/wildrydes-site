2abe8c0240a446af3e0abc70d5b20e4fb15eab17
Sathish Santhanam <sks@Sathishs-MacBook-Pro.local>
2023-12-28 14:20:38 -0800

    update cognito client info

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_co7BJ3m8D', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5a4t047isc7m695giphfqli51g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
